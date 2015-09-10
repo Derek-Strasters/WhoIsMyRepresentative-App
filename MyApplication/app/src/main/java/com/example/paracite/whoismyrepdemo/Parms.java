92e6cb7bf3e139e1da698d8d5384ab12a182a6df
Derek Strasters <paracite.org@gmail.com>
2015-09-10 10:59:09 -0600

    Implemented swiping tabs to hold each function.

---
+++ MyApplication/app/src/main/java/com/example/paracite/whoismyrepdemo/Parms.java
@@ -0,0 +1,12 @@
+package com.example.paracite.whoismyrepdemo;
+
+/**
+ Created by paracite on 9/9/15.
+
+ A collection of unchanging parameters.
+ */
+public class Parms {
+
+ public static final String PREFS = "WIMRD-Preference-File.dat";
+
+}
